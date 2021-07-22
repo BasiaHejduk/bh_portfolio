35741399ee7a69a7d35e64513cac7caf33a413e5
Barbara Hejduk <hejduk.b@gmail.com>
2021-07-22 17:45:47 +0200

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import "./Home.scss"
+
+const Home = () => {
+    return (
+        <div className="home">
+            <p className="home__headline">Front End Developer</p>
+            <p className="home__tagline">Basia Hejduk</p>
+        </div>
+    )
+};
+
+export default Home;
