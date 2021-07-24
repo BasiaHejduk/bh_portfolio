7b5d92d208eff566bc0ac4deb7670a7e2ef8385a
Barbara Hejduk <hejduk.b@gmail.com>
2021-07-24 21:20:16 +0200

    lorem

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 <div className="about__grid">
                     <div className="about__text">
                         <p className="about__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <p className="about__quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        <p className="about__quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
                     </div>
                     <div className="about__image"></div>
                 </div>
