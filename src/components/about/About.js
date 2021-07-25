9b49486a07b02b2e22b9073858e28ad81a48921b
Barbara Hejduk <hejduk.b@gmail.com>
2021-07-25 15:04:59 +0200

    first animations done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,52 @@
+import { useEffect } from "react";
+import gsap from "gsap";
 import "./About.scss";
 
 const About = () => {
+
+    useEffect(() => {
+
+        const technologiesIcons = document.querySelectorAll(".about__technologies-icon");
+        const tl = gsap.timeline({defaults: {ease: "power3.inOut"}});
+       
+        const animateIcons = () => {
+            tl.fromTo(technologiesIcons, {autoAlpha: 0, y: "-=50"}, {duration: 1, autoAlpha: 1, y: "+=50", stagger: 0.1})
+        }
+
+        const touchToAnimate = (entries, ob) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    animateIcons();
+                    ob.unobserve(entry.target);
+                }
+            })
+        };
+
+        let options = {
+            root: null,
+            rootMargin: "-20px"
+        }
+
+        const observerIcons = new IntersectionObserver(touchToAnimate, options);
+        observerIcons.observe(document.querySelector(".about__technologies-grid"))
+
+    }, [])
+
+    const showDetails = (e) => {
+        const detailToShow = e.target.firstElementChild;
+        const tl = gsap.timeline({defaults: {ease: "power3.inOut"}});
+        tl.fromTo(detailToShow, {autoAlpha: 0, rotationY: 180}, {duration: 1, autoAlpha: 1, rotationY: 0});
+    }
+
+    const hideDetails = (e) => {
+        const detailToHide = e.target;
+        const tl = gsap.timeline({defaults: {ease: "power3.inOut"}});
+        tl
+            .add("start")
+            .fromTo(detailToHide, {autoAlpha: 1, rotationY: 0}, {duration: 1, autoAlpha: 1, rotationY: 180})
+            .to(detailToHide, {duration: 2, autoAlpha: 0}, "start")  
+    }
+
     return (
         <div className="about" name="about">
             <div className="wrapper">
@@ -15,13 +61,48 @@ const About = () => {
                 <div className="about__technologies">
                     <p className="about__technologies-title">TECHNOLOGIE:</p>
                     <div className="about__technologies-grid">
-                        <div className="about__technologies-icon about__technologies-icon--html"></div>
-                        <div className="about__technologies-icon about__technologies-icon--css"></div>
-                        <div className="about__technologies-icon about__technologies-icon--js"></div>
-                        <div className="about__technologies-icon about__technologies-icon--sass"></div>
-                        <div className="about__technologies-icon about__technologies-icon--rwd"></div>
-                        <div className="about__technologies-icon about__technologies-icon--react"></div>
-                        <div className="about__technologies-icon about__technologies-icon--git"></div>
+                        <div className="about__technologies-icon about__technologies-icon--html" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details" 
+                                onMouseLeave={(e)=> hideDetails(e)}><span>HTML5</span>
+                            </div>
+                        </div>
+                        <div className="about__technologies-icon about__technologies-icon--css" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details"
+                                onMouseLeave={(e)=> hideDetails(e)}><span>CSS</span>
+                            </div>
+                        </div>
+                        <div className="about__technologies-icon about__technologies-icon--js" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details"
+                                onMouseLeave={(e)=> hideDetails(e)}><span>JAVA <br/>SCRIPT</span>
+                            </div>
+                        </div>
+                        <div className="about__technologies-icon about__technologies-icon--sass" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details"
+                                onMouseLeave={(e)=> hideDetails(e)}><span>SASS</span>
+                            </div>
+                        </div>
+                        <div className="about__technologies-icon about__technologies-icon--rwd" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details"
+                                onMouseLeave={(e)=> hideDetails(e)}><span>RWD</span>
+                            </div>
+                        </div>
+                        <div className="about__technologies-icon about__technologies-icon--react" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details"
+                                onMouseLeave={(e)=> hideDetails(e)}><span>REACT</span>
+                            </div>
+                        </div>
+                        <div className="about__technologies-icon about__technologies-icon--git" 
+                            onMouseEnter={(e)=> showDetails(e)}>
+                            <div className="about__technologies-details"
+                                onMouseLeave={(e)=> hideDetails(e)}><span>GIT</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,12 +7,48 @@ const Menu = () => {
 
     useEffect(() => {
 
+        document.addEventListener("scroll", changeMenuColor);
+
         const menu = document.querySelector(".menu");
         const tl = gsap.timeline({defaults: {ease: "power3.inOut"}});
         gsap.set(menu, {autoAlpha:0});
         tl.fromTo(menu, {rotationX: 45, transformOrigin: "0% -50% 50%"}, {duration: 2, delay: 4, rotationX: 0, autoAlpha: 1})
 
-        document.addEventListener("scroll", changeMenuColor);
+        let options = {
+            root: null,
+            rootMargin: "-50%",
+        };
+
+        const highlightMenuItem = (entries, ob, element) => {
+            const tl = gsap.timeline({defaults: {ease: "power3.inOut"}});
+
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    tl  
+                        .add("start")
+                        .fromTo(element, {rotationX: 360}, {duration: 1, rotationX: 0})
+                        .to(element, {duration: 1, fontWeight: "500"}, "start")
+                } else if (!entry.isIntersecting) {
+                    tl.to(element, {duration: 1, fontWeight: "300"})
+                }
+            })
+        };
+
+        const homeItem = document.querySelector("#home-item")
+        let observerHome = new IntersectionObserver((entries, ob) => highlightMenuItem(entries, ob, homeItem), options);
+        observerHome.observe(document.querySelector(".home"))
+
+        const aboutItem = document.querySelector("#about-item")
+        let observerAbout = new IntersectionObserver((entries, ob) => highlightMenuItem(entries, ob, aboutItem), options);
+        observerAbout.observe(document.querySelector(".about"))
+
+        const projectsItem = document.querySelector("#projects-item")
+        let observerProjects = new IntersectionObserver((entries, ob) => highlightMenuItem(entries, ob, projectsItem), options);
+        observerProjects.observe(document.querySelector(".projects"))
+
+        const contactItem = document.querySelector("#contact-item")
+        let observerContact = new IntersectionObserver((entries, ob) => highlightMenuItem(entries, ob, contactItem), options);
+        observerContact.observe(document.querySelector(".contact"))
 
     },[]);
 
@@ -34,10 +70,10 @@ const Menu = () => {
     return (
         <div className="menu">
             <ul className="menu__list">
-                <li className="menu__item" onClick={()=> handleOnClickMenu("home")}>HOME</li>
-                <li className="menu__item" onClick={()=> handleOnClickMenu("about")}>O MNIE</li>
-                <li className="menu__item" onClick={()=> handleOnClickMenu("projects")}>PROJEKTY</li>
-                <li className="menu__item" onClick={()=> handleOnClickMenu("contact")}>KONTAKT</li>
+                <li id="home-item" className="menu__item" onClick={()=> handleOnClickMenu("home")}>HOME</li>
+                <li id="about-item" className="menu__item" onClick={()=> handleOnClickMenu("about")}>O MNIE</li>
+                <li id="projects-item" className="menu__item" onClick={()=> handleOnClickMenu("projects")}>PROJEKTY</li>
+                <li id="contact-item" className="menu__item" onClick={()=> handleOnClickMenu("contact")}>KONTAKT</li>
             </ul>
         </div>
     )
